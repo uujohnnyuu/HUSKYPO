93f5e29dc88b3bb62438e1ab1906d92785b7fe18
johnny_aws <johnny071531@gmail.com>
2024-06-27 09:44:20 +0800

    update logconfig 2

--- huskypo/logconfig.py
+++ huskypo/logconfig.py
@@ -10,24 +10,24 @@ import logging
 
 
 def basic(
-    file: str = './log.log', 
-    level: int = logging.INFO, 
+    file: str = './log.log',
+    level: int = logging.INFO,
     format: str = '%(asctime)s |%(levelname)s| |[%(filename)s:%(lineno)d][%(funcName)s]| %(message)s',
-    datefmt: str ='%Y-%m-%d %H:%M:%S',
+    datefmt: str = '%Y-%m-%d %H:%M:%S',
     filemode: str = 'w'
 ) -> None:
     """
-    This function calls the logging.basicConfig() method and 
-    creates directories according to the file path. 
-    If you accept these default parameter settings, 
+    This function calls the logging.basicConfig() method and
+    creates directories according to the file path.
+    If you accept these default parameter settings,
     you can use this method to quickly set them up.
 
     Args:
-	- file: Relative or absolute path of the file.
-	- level: Same as the level parameter in basicConfig.
-	- format: Same as the format parameter in basicConfig.
-	- datefmt: Same as the datefmt parameter in basicConfig.
-	- filemode: Same as the filemode parameter in basicConfig.
+        - file: Relative or absolute path of the file.
+        - level: Same as the level parameter in basicConfig.
+        - format: Same as the format parameter in basicConfig.
+        - datefmt: Same as the datefmt parameter in basicConfig.
+        - filemode: Same as the filemode parameter in basicConfig.
     """
     abspath = os.path.abspath(file)
     dirname = os.path.dirname(abspath)
